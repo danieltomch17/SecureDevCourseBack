7c9937a0c9909b3b27faa2499259af3c23d573b2
danielt-DI <daniel.tomchin@deepinstinct.com>
2023-05-05 13:35:44 +0300

    Create skeleton

--- Server/app/views.py
+++ Server/app/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def register(request):
+    return HttpResponse("Register")
+
+
+def login(request):
+    return HttpResponse("Login")
+
+
+def change_pass(request):
+    return HttpResponse("Change pass")
+
+
+def add_client(request):
+    return HttpResponse("Add new client")
+
+
+def gen_otp(request):
+    return HttpResponse("Generate one-time password")
+
+
+def verify_otp(request):
+    return HttpResponse("Verify one-time password")
